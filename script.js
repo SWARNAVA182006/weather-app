ec92497ab1c5da75706360e207a884e8fc725e6c
SWARNAVA SARKAR <swarnava2019@gmail.com>
2025-10-19 03:33:50 +0530

    Updated API key and fixed weather functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* Final robust script.js (Bootstrap UI) */
-const apiKey = '2ecae6763c9bcb92dd08c37f165b10ba';
+const apiKey = '34407a88364f4a6293e220200251810';
 
 const searchBtn = document.getElementById('searchBtn');
 const cityInput  = document.getElementById('cityInput');
